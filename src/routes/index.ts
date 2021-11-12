a1c5915b3d49c4cb41205e139a791436770c5bd2
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2021-11-11 22:25:34 -0400

    feat: create sessions to users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import categoriesRouter from './categories.routes';
+import sessionsRouter from './sessions.routes';
 import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/categories', categoriesRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
