74fd42bca259c8ea7f68f8048ef749178191544d
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2021-11-14 19:40:02 -0400

    feat: add cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
@@ -11,6 +12,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
