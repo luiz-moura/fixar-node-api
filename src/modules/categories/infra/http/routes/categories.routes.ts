6820f0f52ffe942f692a4e3b6cf1dd8e2c8e9662
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2021-11-20 01:06:00 -0400

    feat: validate data on routes

--- src/modules/categories/infra/http/routes/categories.routes.ts
+++ src/modules/categories/infra/http/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import CategoriesController from '../controllers/CategoriesController';
@@ -6,7 +7,17 @@ import CategoriesController from '../controllers/CategoriesController';
 const categoriesRouter = Router();
 const categoriesController = new CategoriesController();
 
-categoriesRouter.post('/', ensureAuthenticated, categoriesController.create);
+categoriesRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      slug: Joi.string(),
+    },
+  }),
+  ensureAuthenticated,
+  categoriesController.create,
+);
 categoriesRouter.get('/', categoriesController.index);
 
 export default categoriesRouter;
