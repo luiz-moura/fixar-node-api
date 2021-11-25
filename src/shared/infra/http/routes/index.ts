b4428cc0eaddc087989e3918b7ad36045273a1da
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2021-11-25 19:52:04 -0400

    chore: general update on routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,8 +6,9 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import platformsRouter from '@modules/platforms/infra/http/routes/platforms.routes';
 import instructorsRouter from '@modules/instructors/infra/http/routes/instructors.routes';
-import CoursesRouter from '@modules/courses/infra/http/routes/courses.routes';
-import RatingsRouter from '@modules/ratings/infra/http/routes/ratings.routes';
+import coursesRouter from '@modules/courses/infra/http/routes/courses.routes';
+import ratingsRouter from '@modules/ratings/infra/http/routes/ratings.routes';
+import coursesPendingRouter from '@modules/courses/infra/http/routes/coursesPending.routes';
 
 const routes = Router();
 
@@ -17,7 +18,8 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/profile', profileRouter);
 routes.use('/platforms', platformsRouter);
 routes.use('/instructors', instructorsRouter);
-routes.use('/courses', CoursesRouter);
-routes.use('/ratings', RatingsRouter);
+routes.use('/courses', coursesRouter);
+routes.use('/ratings', ratingsRouter);
+routes.use('/courses-pending', coursesPendingRouter);
 
 export default routes;
